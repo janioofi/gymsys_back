4f9ad13657cbe2a8390404d1a4eed8ad50d46d75
Jânio <janioofi@gmail.com>
2024-04-07 19:11:18 -0300

    configuração dos endpoints de usuario e criação do cloudgateway

---
+++ ms-cloudgateway/src/main/java/br/janioofi/mscloudgateway/MsCloudgatewayApplication.java
@@ -0,0 +1,26 @@
+package br.janioofi.mscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsCloudgatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCloudgatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder){
+        return builder
+                .routes()
+                .route(r -> r.path("/api/v1/usuarios/**").uri("lb://ms-gym"))
+                .build();
+    }
+
+}
---
+++ ms-cloudgateway/src/test/java/br/janioofi/mscloudgateway/MsCloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.janioofi.mscloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCloudgatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
